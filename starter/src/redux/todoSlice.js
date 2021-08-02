0a003bbcb5cf83275b4e835dd35658cd577cacc0
Nauzet Hernández <nauzet@adedelivered.com>
2021-08-02 11:03:26 +0100

    ADD: Provider, Store, Slice

---
+++ starter/src/redux/todoSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState: [
+    { id: 1, title: "todo1", completed: false },
+    { id: 2, title: "todo2", completed: false },
+    { id: 3, title: "todo3", completed: true },
+    { id: 44, title: "todo44", completed: false },
+  ],
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: Date.now(),
+        title: action.payload.title,
+        complete: false,
+      };
+      state.push(newTodo);
+    },
+    toogleComplete: (state, action) => {
+      const index = state.findIndex((todo) => todo.id === action.payload.id);
+      state[index].completed = action.payload.completed;
+    },
+    deleteTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload.id);
+    },
+  },
+});
+
+export const { addTodo, toogleComplete, deleteTodo } = todoSlice.actions;
+export default todoSlice.reducer;
